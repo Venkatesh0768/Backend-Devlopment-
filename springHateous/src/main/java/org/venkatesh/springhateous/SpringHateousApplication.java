3f8b56c10eb69e64cb5ab675ac98a8c5010dc8af
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-11-24 23:30:28 +0530

    Eureka , AmdinService , Feignservice , LoadBalanced

---
+++ springHateous/src/main/java/org/venkatesh/springhateous/SpringHateousApplication.java
@@ -0,0 +1,13 @@
+package org.venkatesh.springhateous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHateousApplication.class, args);
+    }
+
+}
---
+++ springHateous/src/test/java/org/venkatesh/springhateous/SpringHateousApplicationTests.java
@@ -0,0 +1,13 @@
+package org.venkatesh.springhateous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHateousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
